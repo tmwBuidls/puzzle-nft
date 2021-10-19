50fef5ab1bda6ed7b9a80db162d31cab87e8b00e
Tom Watson <tom@productgarden.io>
2021-10-19 18:58:25 +0200

    fix too many arguments for old constructor error

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,6 @@
 const main = async () => {
   const nftContractFactory = await hre.ethers.getContractFactory('Puzzle');
-  const nftContract = await nftContractFactory.deploy('test');
+  const nftContract = await nftContractFactory.deploy();
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
 };
