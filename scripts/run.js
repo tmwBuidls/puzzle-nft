9b7145355648895cf1e5d294f65785aef1fd01aa
Tom Watson <tom@productgarden.io>
2021-10-19 18:57:27 +0200

    remove the ipfs link for now

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,6 @@
 const main = async () => {
   const nftContractFactory = await hre.ethers.getContractFactory('Puzzle');
-  const nftContract = await nftContractFactory.deploy('ipfs://Qmc2cjnm3xdyPoY4X82uLUrfeb6KdabxKTvoKHRcrGRRR9/');
+  const nftContract = await nftContractFactory.deploy('test');
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
 };
